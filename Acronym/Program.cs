61a9407161dfa4731777b99ba921a5933e7f100e
Tenzin Wangdu <tw1270@ymail.com>
2020-11-03 23:24:42 -0500

    Day2 Printing the Acronym

---
+++ Acronym/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Multi_Term
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a multi word term");
+            string input = Console.ReadLine().ToUpper();
+            string[] x = input.Split(' ');
+            foreach(var y in x)
+            {
+                Console.WriteLine($"{y[0]}");
+            }
+
+        }
+    }
+}
